ea616cd04b6c0588cc532c9c4cfa166656f640b7
Christian Schwarz <christian.schwarz@hexagongeospatial.com>
2015-09-15 10:26:04 +0200

    Added Wkx.TestGenerator project

---
+++ Wkx.TestGenerator/GeometryFormat.cs
@@ -0,0 +1,32 @@
+﻿namespace Wkx.TestGenerator
+{
+    class GeometryFormat
+    {
+        public string Name { get; private set; }
+        public string SqlAsFunction { get; private set; }
+        public string SqlFromFunction { get; private set; }
+        public bool IsBinary { get; private set; }
+        public string SqlAdditionalFlags { get; private set; }
+
+        public GeometryFormat(string name, string sqlAsFunction, string sqlFromFunction, bool isBinary, string sqlAdditionalFlags = null)
+        {
+            Name = name;
+            SqlAsFunction = sqlAsFunction;
+            SqlFromFunction = sqlFromFunction;
+            IsBinary = isBinary;
+            SqlAdditionalFlags = sqlAdditionalFlags;
+        }
+
+        public string GenerateSql()
+        {
+            string targetSql = string.Format("{0}(ST_GeomFromText(@input, 4326){1})", SqlAsFunction, string.IsNullOrEmpty(SqlAdditionalFlags) ? string.Empty : string.Concat(", ", SqlAdditionalFlags));
+
+            string targetAsText = string.Format("ST_AsText({0}({1}))", SqlFromFunction, targetSql);
+
+            if (IsBinary)
+                targetSql = string.Format("encode({0}, 'hex')", targetSql);
+
+            return string.Format("{0} {1}, {2} {1}result", targetSql, Name, targetAsText);
+        }
+    }
+}
---
+++ Wkx.TestGenerator/Program.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Npgsql;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Wkx.TestGenerator
+{
+    class Program
+    {
+        static IEnumerable<GeometryFormat> geometryFormats = new List<GeometryFormat>()
+        {
+            new GeometryFormat("wkt", "ST_AsText", "ST_GeomFromText", false),
+            new GeometryFormat("ewkt", "ST_AsEWKT", "ST_GeomFromEWKT", false),
+            new GeometryFormat("wkb", "ST_AsBinary", "ST_GeomFromWKB", true),
+            new GeometryFormat("ewkb", "ST_AsEWKB", "ST_GeomFromEWKB", true),
+            new GeometryFormat("wkbXdr", "ST_AsBinary", "ST_GeomFromWKB", true, "'xdr'"),
+            new GeometryFormat("ewkbXdr", "ST_AsEWKB", "ST_GeomFromEWKB", true, "'xdr'")
+            //new GeometryFormat("twkb", "ST_AsTWKB", "ST_GeomFromTWKB", true),
+            //new GeometryFormat("geojson", "ST_AsGeoJSON", "ST_GeomFromGeoJSON", false)
+        };
+
+        static void Main(string[] args)
+        {
+            JObject testInputData = JsonConvert.DeserializeObject<JObject>(File.ReadAllText("tests.input.json"));
+            JObject testOutputData = new JObject();
+
+            using (NpgsqlConnection connection = new NpgsqlConnection("Server=localhost;Port=5432;User Id=postgres;Password=postgres"))
+            {
+                connection.Open();
+
+                using (NpgsqlCommand command = new NpgsqlCommand())
+                {
+                    command.Connection = connection;
+
+                    foreach (var testInput in testInputData)
+                    {
+                        command.CommandText = GenerateTestSql(geometryFormats);
+
+                        command.Parameters.Clear();
+                        command.Parameters.Add(new NpgsqlParameter("input", testInput.Value.Value<string>()));
+
+                        using (NpgsqlDataReader reader = command.ExecuteReader())
+                        {
+                            reader.Read();
+
+                            JObject testOutput = new JObject();
+                            JObject testOutputResult = new JObject();
+
+                            testOutputData[testInput.Key] = testOutput;
+
+                            foreach (GeometryFormat geometryFormat in geometryFormats)
+                            {
+                                testOutput[geometryFormat.Name] = reader.GetString(reader.GetOrdinal(geometryFormat.Name));
+
+                                string result = reader.GetString(reader.GetOrdinal(geometryFormat.Name + "result"));
+
+                                if (testInput.Value.Value<string>() != result)
+                                    testOutputResult[geometryFormat.Name] = result;
+                            }
+
+                            if (testOutputResult.Count > 0)
+                                testOutput["results"] = testOutputResult;
+                        }
+                    }
+                }
+            }
+
+            File.WriteAllText("testdata.json", JsonConvert.SerializeObject(testOutputData, Formatting.Indented));
+        }
+
+        private static string GenerateTestSql(IEnumerable<GeometryFormat> geometryFormats)
+        {
+            return "SELECT " + string.Join(", \r\n", geometryFormats.Select(t => t.GenerateSql()));
+        }
+    }
+}
